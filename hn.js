76f8becc8e6401d051af35dbf9242f4058994ea7
lakshay <lakshayakalra29@gmail.com>
2023-05-06 22:55:58 +0530

    changed buttons

--- hn.js
+++ hn.js
@@ -2,20 +2,23 @@ const wrapper = document.querySelector('.wrapper');
 const question = document.querySelector('.question');
 const yesBtn = document.querySelector('.yes-btn');
 const noBtn = document.querySelector('.no-btn');
-  const wrapperRect = wrapper.getBoundingClientRect();
-  const noBtnRect = noBtn.getBoundingClientRect();
+const goBack = document.querySelector('.go-back');
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
 
- yesBtn.addEventListener('click' ,() =>{
-    question.innerHTML = 'i know it :) ';
- });
- noBtn.addEventListener('click' ,() =>{
-    question.innerHTML = ' ';
+goBack.remove();
+
+const back = yesBtn.addEventListener('click' ,() =>{
+   question.innerHTML = 'I knew it :) ';
+   noBtn.remove();
 });
-    noBtn.addEventListener('mouseover' , () =>{  
-    const i = Math.floor(Math.random()  * (wrapperRect.width -  noBtnRect.width)) 
-     +1 ;
-     const j = Math.floor(Math.random()  * (wrapperRect.height -  noBtnRect.height)) 
-     +1 ;
-     noBtn.style.right = i + 'px';
-     noBtn.style.top = j + "px";
-     });
+
+
+noBtn.addEventListener('mouseover' , () =>{  
+   const i = Math.floor(Math.random()  * (wrapperRect.width -  noBtnRect.width)) 
+   +1 ;
+   const j = Math.floor(Math.random()  * (wrapperRect.height -  noBtnRect.height)) 
+   +1 ;
+   noBtn.style.right = i + 'px';
+   noBtn.style.top = j + "px";
+});
